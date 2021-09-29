19faa9660e3fa7761163b500e85fc9d1d4f95d3a
Symphon-y <90964291+Symphon-y@users.noreply.github.com>
2021-09-29 13:46:37 -0500

    Added: Random Button

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("random");
+// var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+// var randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
 
 var colorPicker = function(){
     body.style.background =
@@ -14,7 +17,49 @@ var colorPicker = function(){
     css.textContent = "Hex Values: " + color1.value + " " + color2.value + ";";
 };
 
+var btnColorPicker = function(){
+   button.style.background =
+    "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value 
+    + ")";
+
+    css.textContent = "Hex Values: " + color1.value + " " + color2.value + ";";
+};
+
+var btnColorPickerRandom = function(){
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+    button.style.background =
+     "linear-gradient(to right, " 
+     + randomColor
+     + ", " 
+     + randomColor2 
+     + ")";
+ 
+     css.textContent = "Hex Values: " + randomColor + " " + randomColor2 + ";";
+ };
+
+var randomPicker = function(){
+    var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    var randomColor2 = '#'+Math.floor(Math.random()*16777215).toString(16);
+    body.style.background =
+    "linear-gradient(to right, " 
+    + randomColor
+    + ", " 
+    + randomColor2 
+    + ")";
+
+    css.textContent = "Hex Values: " + randomColor + " " + randomColor2 + ";";
+};
+
+button.addEventListener("click", randomPicker);
+button.addEventListener("click", randomPicker);
+
 color1.addEventListener("input", colorPicker);
+color1.addEventListener("input", btnColorPicker);
 
 color2.addEventListener("input", colorPicker);
+color2.addEventListener("input", btnColorPicker);
 
